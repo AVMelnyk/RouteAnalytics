adc100b818b98c5b2d824d132733a4c7240e716f
AVMelnyk <melnyk.andreew@gmail.com>
2017-05-13 15:51:21 +0300

    added Route model

---
+++ src/main/java/ua/pp/avmelnyk/routeanalytics/model/Route.java
@@ -0,0 +1,33 @@
+package ua.pp.avmelnyk.routeanalytics.model;
+
+import java.util.ArrayList;
+
+public class Route {
+    private String routeNumber;
+    private String routeName;
+    private ArrayList routeStops;
+
+    public String getRouteNumber() {
+        return routeNumber;
+    }
+
+    public void setRouteNumber(String routeNumber) {
+        this.routeNumber = routeNumber;
+    }
+
+    public String getRouteName() {
+        return routeName;
+    }
+
+    public void setRouteName(String routeName) {
+        this.routeName = routeName;
+    }
+
+    public ArrayList getRouteStops() {
+        return routeStops;
+    }
+
+    public void setRouteStops(ArrayList routeStops) {
+        this.routeStops = routeStops;
+    }
+}
